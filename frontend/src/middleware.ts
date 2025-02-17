501d71719742f102670dc7a16b69767ba575afa5
João Guilherme Fernandes Borba <168598901+JoaoGuilherme527@users.noreply.github.com>
2025-02-17 02:55:56 -0300

    Update middleware.ts

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,13 +1,13 @@
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
-// import { getUserMeLoader } from "./data/services/get-user-me-loader";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getUserMeLoader } from "./data/services/get-user-me-loader";
 
-export async function middleware() {
-//   // const user = await getUserMeLoader();
-//   // const currentPath = request.nextUrl.pathname;
+export async function middleware(request: NextRequest) {
+    const user = await getUserMeLoader();
+    const currentPath = request.nextUrl.pathname;
 
-//   // if (currentPath.startsWith("/dashboard") && user.ok === false) {
-//   //   return NextResponse.redirect(new URL("/signin", request.url));
-//   // }
-//   // return NextResponse.next();
+    if (currentPath.startsWith("/dashboard") && user.ok === false) {
+        return NextResponse.redirect(new URL("/signin", request.url));
+    }
+    return NextResponse.next();
 }
